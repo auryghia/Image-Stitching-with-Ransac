5fc8ea4f19723f3823299bc9f54ab8b4671d312e
auryghia <auryghia05@gmail.com>
2025-04-23 18:56:53 +0200

    experiments

--- corners.py
+++ corners.py
@@ -1,4 +1,4 @@
-from utils import compute_gradients
+from skimage.feature import corner_harris, corner_peaks
 import numpy as np
 import cv2
 
@@ -8,6 +8,39 @@ Sx = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
 Sy = Sx.T
 
 G = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]]) / 16
+
+
+def harris_corners(
+    image, sigma=1, k=0.005, min_distance=1, size=10, plot=False, image_name="Image"
+):
+    """
+    Harris corner detection using skimage's corner_harris and corner_peaks.
+    Parameters:
+    - image: Input image (grayscale).
+    - sigma: Gaussian filter standard deviation.
+    - k: Harris detector free parameter.
+    - min_distance: Minimum distance between detected corners.
+    - size: Size of the Gaussian filter.
+    - plot: If True, plot the detected corners.
+    - image_name: Name of the image for plotting title.
+    """
+
+    harris_response = corner_harris(image, sigma=sigma)
+    keypoints = corner_peaks(
+        harris_response,
+        exclude_border=size // 2,
+        min_distance=min_distance,
+        threshold_rel=k,
+    )
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.imshow(image, cmap="gray")
+        ax.plot(keypoints[:, 1], keypoints[:, 0], "r.", markersize=2)
+        ax.set_title(f"Harris Corners - {image_name}")
+        plt.show()
+
+    return keypoints
 
 
 def corner_response(image, k, Sx=Sx, Sy=Sy, G=G):
